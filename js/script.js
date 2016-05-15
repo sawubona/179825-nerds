a0560353f362e282e802d265c87cf8b7d361f2b4
sawubona <sawubona36@gmail.com>
2016-05-15 13:35:41 +0300

    Устранил замечания добавил минифицированные версии css, js

--- js/script.js
+++ js/script.js
@@ -32,3 +32,20 @@ window.addEventListener("keydown", function (event) {
         }
     }
 });
+
+function initialize() {
+    var mapOptions = {
+      zoom: 17,
+      center: new google.maps.LatLng(59.939116, 30.320012)
+    }
+    var map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
+
+    var image = "img/map-point.png"; 
+    var myLatLng = new google.maps.LatLng(59.938568, 30.323671);
+    var beachMarker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        icon: image
+    });
+}
+google.maps.event.addDomListener(window, "load", initialize);
